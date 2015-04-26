0d671581f661b36f905441c8f25b92a373d29ed5
JesterXL <jesse@jessewarden.com>
2015-04-26 10:27:56 -0400

    fixed gulp copy: 1. gulp-merge does NOT work 2. browserSync.stream mode does NOT work 3. Bluebird does

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,32 @@
-var gulp 		= require('gulp');
-var browserSync = require('browser-sync');
-var del 		= require('del');
-var vinylPaths  = require('vinyl-paths');
-var typescript 	= require('gulp-typescript');
-var concat 		= require('gulp-concat');
-var tsify		= require('tsify');
-var browserify	= require('browserify');
-var source 		= require('vinyl-source-stream');
-var buffer 		= require('vinyl-buffer');
-var wiredep 	= require('wiredep').stream;
-var inject 		= require('gulp-inject');
-var nodemon 	= require('gulp-nodemon');
-var jshint 		= require('gulp-jshint');
-var jscs 		= require('gulp-jscs');
-var complexity 	= require('gulp-complexity');
-var karma 		= require('karma').server;
-var stylish 	= require('jshint-stylish');
-var eslint 		= require('gulp-eslint');
+var gulp                = require('gulp');
+var browserSync         = require('browser-sync');
+var del                 = require('del');
+var vinylPaths          = require('vinyl-paths');
+var typescript          = require('gulp-typescript');
+var concat              = require('gulp-concat');
+var tsify               = require('tsify');
+var browserify          = require('browserify');
+var source              = require('vinyl-source-stream');
+var buffer              = require('vinyl-buffer');
+var wiredep             = require('wiredep').stream;
+var inject              = require('gulp-inject');
+var nodemon             = require('gulp-nodemon');
+var jshint              = require('gulp-jshint');
+var jscs                = require('gulp-jscs');
+var complexity          = require('gulp-complexity');
+var karma               = require('karma').server;
+var stylish             = require('jshint-stylish');
+var eslint              = require('gulp-eslint');
 var eslintPathFormatter = require('eslint-path-formatter');
-var mocha 		= require('gulp-mocha');
-var  mochaLcovReporter = require('mocha-lcov-reporter');
-var coverage = require('gulp-coverage');
-var open = require('gulp-open');
-var istanbul = require('gulp-istanbul');
-// var Promise = require('Bluebird');
-var merge = require('gulp-merge');
+var mocha               = require('gulp-mocha');
+var  mochaLcovReporter  = require('mocha-lcov-reporter');
+var coverage            = require('gulp-coverage');
+var open                = require('gulp-open');
+var istanbul            = require('gulp-istanbul');
+// var Promise          = require('Bluebird');
+var merge               = require('gulp-merge');
 
-var CONFIG 		= require('./build.config');
+var CONFIG              = require('./build.config');
 
 gulp.task('hello', function()
 {
@@ -128,32 +128,77 @@ gulp.task('clean', function(done)
         });
 });
 
-gulp.task('copyIndex', ['clean'], function()
+gulp.task('cow', function()
 {
-	return gulp.src('src/client/index.html')
-	.pipe(wiredep({ignorePath: "../../"}))
-	.pipe(gulp.dest('./build'))
-	.pipe(browserSync.reload({stream: true}));
+    return new Promise(function(resolve, reject)
+    {
+        setTimeout(resolve, 1000);
+    })
+    .then(function()
+    {
+        return new Promise(function(resolve, reject)
+        {
+            setTimeout(resolve, 1000);
+        });
+    })
+    .then(function()
+    {
+        return new Promise(function(resolve, reject)
+        {
+            setTimeout(resolve, 1000);
+        });
+    });
 });
 
-gulp.task('inject', ['copyIndex'], function()
+gulp.task('copy', ['clean'], function()
+{
+    // NOTE: doesn't work, task never completes, I give up.
+    // All streams above work fine if you return individually,
+    // so it's something wrong with merge.
+    // return merge(htmlStream, jsStream, templateStream);
+    
+    return new Promise(function(resolve, reject)
+    {
+        gulp.src('src/client/index.html')
+        .pipe(wiredep({ignorePath: "../../"}))
+        .pipe(gulp.dest('./build'))
+        .on('end', resolve)
+        .on('error', reject);
+    })
+    .then(function()
+    {
+        return new Promise(function(resolve, reject)
+        {
+             gulp.src(CONFIG.client.sourceFiles)
+            .pipe(gulp.dest('./build'))
+            .on('end', resolve)
+            .on('error', reject);
+        });
+    })
+    .then(function()
+    {
+        return new Promise(function(resolve, reject)
+        {
+             gulp.src(CONFIG.client.templateFiles)
+            .pipe(gulp.dest('./build'))
+            .on('end', resolve)
+            .on('error', reject);
+        });
+    })
+    .then(function()
+    {
+        // NOTE: this guy, even in stream mode, breaks, so putting here.
+        browserSync.reload();
+    });
+});
+
+gulp.task('inject', ['copy'], function()
 {
 	var sources = gulp.src(CONFIG.client.sourceFiles, {read: false});
 	return gulp.src('./build/index.html')
 	.pipe(inject(sources, {ignorePath: '/src/client/'}))
 	.pipe(gulp.dest('./build'))
 	.pipe(browserSync.reload({stream: true}));
-});
-
-gulp.task('copy', ['clean'], function()
-{
-	var jsStream = gulp.src(CONFIG.client.sourceFiles)
-    .pipe(gulp.dest('./build'));
-
-    var templateStream = gulp.src(CONFIG.client.templateFiles)
-    .pipe(gulp.dest('./build'));
-
-    return merge(jsStream, templateStream);
 });
 
 gulp.task('browserSync', function(done)
@@ -164,24 +209,30 @@ gulp.task('browserSync', function(done)
 	done();
 });
 
-gulp.task('watch', function()
-{
-	gulp.watch([
-        'src/client/index.html', 
-        'src/client/**/*.js'], 
-        ['clean', 'copy', 'copyIndex', 'inject'])
-    .on('change', function(sup)
-    {
-        browserSync.reload();
-    });
-});
+// gulp.task('watch', function(done)
+// {
+// 	gulp.watch([
+//         'src/client/*.html', 
+//         'src/client/*.js', 
+//         'src/client/**/*.js',
+//         'src/client/**/*.html'], 
+//         {read: false},
+//         ['clean', 'copy', 'inject'])
+//     .on('change', function(sup)
+//     {
+//         console.log
+//     });
+//     done();
+// });
 
 gulp.task('start', function (done)
 {
   nodemon({
 	    script: 'src/static/app.js',
 	    ext: 'js html',
-	  	env: { 'NODE_ENV': 'development' }
+        ignore: ['Gruntfile.js', 'gulpfile.js'],
+	  	env: { 'NODE_ENV': 'development' },
+        tasks: ['clean', 'copy', 'inject']
   	});
   done();
 });
@@ -189,9 +240,7 @@ gulp.task('start', function (done)
 gulp.task('default', [
 	'clean', 
 	'copy', 
-	'copyIndex', 
 	'inject', 
 	'browserSync', 
-    'watch',
 	'start'
 ]);
