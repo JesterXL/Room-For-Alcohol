04e5b5c83d0ea8514107925cef38257993ca2bb0
JesterXL <jesse@jessewarden.com>
2015-05-11 12:20:26 -0400

    added working unit tests

--- src/client/main/workout/workoutList/workoutList.controller.spec.js
+++ src/client/main/workout/workoutList/workoutList.controller.spec.js
@@ -1,10 +1,10 @@
-/* jshint -W117, -W030 */
+// /* jshint -W117, -W030 */
 'use strict';
 describe('Workout List', function()
 {
-    describe('Workout List Controller', function()
+    describe('Controller', function()
     {
-        var scope, controller, $httpBackend;
+        var scope, controller, $httpBackend, workoutModel;
 
         beforeEach(function()
         {
@@ -13,73 +13,10 @@ describe('Workout List', function()
 
         beforeEach(function()
         {
-          inject(function($rootScope, $controller, _$httpBackend_)
+          inject(function($rootScope, $controller, _$httpBackend_, _workoutModel_)
           {
-            $httpBackend = _$httpBackend_;
-            var WORKOUT_URL = 'http://'+window.location.hostname+':2146/api/workouts';
-            $httpBackend.expectGET(WORKOUT_URL);
-            $httpBackend.whenGET(WORKOUT_URL).respond([
-        {
-            days: [1, 3, 5],
-            notes: null,
-            exercises: [
-                {
-                    name: 'Squat',
-                    sets: [
-                        {
-                            reps: 5,
-                            weight: 295,
-                            actualReps: 5
-                        },
-                        {
-                            reps: 5,
-                            weight: 295,
-                            actualReps: 5
-                        },
-                        {
-                            reps: 5,
-                            weight: 295,
-                            actualReps: 5
-                        }
-                    ]
-                },
-
-                {
-                    name: 'Bench Press',
-                    sets: [
-                        {
-                            reps: 5,
-                            weight: 180,
-                            actualReps: 5
-                        },
-                        {
-                            reps: 5,
-                            weight: 180,
-                            actualReps: 5
-                        }
-                    ]
-                },
-
-                {
-                    name: 'Deadlift',
-                    sets: [
-                        {
-                            reps: 5,
-                            weight: 315,
-                            actualReps: 5
-                        }
-                    ]
-                }
-            ]
-        },
-        
-        {
-            days: [0, 2, 4, 6],
-            notes: null,
-            exercises: []
-        }
-    ]);
             scope = $rootScope.$new();
+            workoutModel = _workoutModel_;
             controller = $controller('jxlWorkoutListController', {
               $scope: scope
             });
@@ -92,17 +29,87 @@ describe('Workout List', function()
             expect(controller).to.be.defined;
         });
 
-        it('workouts are null by default', function()
+        it('hasWorkoutForToday is false by default', function()
         {
-            expect(controller.workouts).to.not.exist;
+            expect(controller.hasWorkoutForToday).to.be.false;
         });
 
-        it('should have workouts if you flush immediately or cached', function()
+        it('todaysWorkout should be null by default', function()
         {
-            $httpBackend.flush();
-            expect(controller.workouts).to.exist;
-            expect(controller.workouts).to.not.be.empty;
-            expect(controller.workouts).to.have.length.above(0);
+            expect(controller.todaysWorkout).to.not.exist;
         });
+
+        describe('with preset workouts for all days', function()
+        {
+        	beforeEach(function()
+        	{
+        		workoutModel.workouts = [
+	            {
+	                days: [0, 1, 2, 3, 4, 5, 6],
+	                notes: null,
+	                exercises: [
+	                    {
+	                        name: 'Squat',
+	                        sets: [
+	                            {
+	                                reps: 5,
+	                                weight: 295,
+	                                actualReps: 5
+	                            },
+	                            {
+	                                reps: 5,
+	                                weight: 295,
+	                                actualReps: 5
+	                            },
+	                            {
+	                                reps: 5,
+	                                weight: 295,
+	                                actualReps: 5
+	                            }
+	                        ]
+	                    },
+
+	                    {
+	                        name: 'Bench Press',
+	                        sets: [
+	                            {
+	                                reps: 5,
+	                                weight: 180,
+	                                actualReps: 5
+	                            },
+	                            {
+	                                reps: 5,
+	                                weight: 180,
+	                                actualReps: 5
+	                            }
+	                        ]
+	                    },
+
+	                    {
+	                        name: 'Deadlift',
+	                        sets: [
+	                            {
+	                                reps: 5,
+	                                weight: 315,
+	                                actualReps: 5
+	                            }
+	                        ]
+	                    }
+	                ]
+	            }
+	            ];
+        	});
+
+			it('todaysWorkout should NOT be null if we have some workouts for all days setup beforehand', function()
+	        {
+	            expect(controller.todaysWorkout).to.exist;
+	        });
+
+			it('hasWorkoutForToday is false by default', function()
+	        {
+	            expect(controller.hasWorkoutForToday).to.be.true;
+	        });
+        });
+        
     });
 });
